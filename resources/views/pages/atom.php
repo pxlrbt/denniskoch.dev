033ee8109b8c6ddaaedd60a81c419f5affaceade
Dennis Koch <info@pixelarbeit.de>
2025-11-27 19:19:30 +0100

    Encode atom feed

--- resources/views/pages/atom.php
+++ resources/views/pages/atom.php
@@ -10,28 +10,29 @@
     echo '<?xml version="1.0" encoding="utf-8"?>';
 ?>
 <feed xmlns="http://www.w3.org/2005/Atom">
-    <title>denniskoch.dev</title>
+    <title><?= htmlspecialchars('denniskoch.dev', ENT_XML1 | ENT_COMPAT, 'UTF-8') ?></title>
     <updated><?= $latestArticle->updated_at->format(DATE_RFC3339) ?></updated>
     <id>https://denniskoch.dev/feed</id>
     <link rel="self" type="application/atom+xml" href="https://denniskoch.dev/feed"/>
-    <description>Articles about Laravel, Filament, PHP and web development.</description>
+    <description><?= htmlspecialchars('Articles about Laravel, Filament, PHP and web development.', ENT_XML1 | ENT_COMPAT, 'UTF-8') ?></description>
     <author>
-        <name>Dennis Koch</name>
+        <name><?= htmlspecialchars('Dennis Koch', ENT_XML1 | ENT_COMPAT, 'UTF-8') ?></name>
         <uri>https://denniskoch.dev</uri>
     </author>
 
-    <?php foreach ($articles as $article): ?>
-        <entry>
-            <title><?= urldecode($article->title) ?></title>            <id><?= $article->url() ?></id>
-            <link rel="alternate" type="text/html" href="<?= $article->url() ?>"/>
-            <published><?= $article->created_at->format(DATE_RFC3339) ?></published>
-            <updated><?= $article->updated_at->format(DATE_RFC3339) ?></updated>
-            <summary type="text"><?= $article->description ?></summary>
-            <content type="html"><![CDATA[<?= $article->content ?>]]></content>
-            <author>
-                <name>Dennis Koch</name>
-                <uri>https://denniskoch.dev</uri>
-            </author>
-        </entry>
-    <?php endforeach; ?>
+<?php foreach ($articles as $article): ?>
+    <entry>
+        <title><?= htmlspecialchars(urldecode($article->title), ENT_XML1 | ENT_COMPAT, 'UTF-8') ?></title>
+        <id><?= htmlspecialchars($article->url(), ENT_XML1 | ENT_COMPAT, 'UTF-8') ?></id>
+        <link rel="alternate" type="text/html" href="<?= htmlspecialchars($article->url(), ENT_XML1 | ENT_COMPAT, 'UTF-8') ?>"/>
+        <published><?= $article->created_at->format(DATE_RFC3339) ?></published>
+        <updated><?= $article->updated_at->format(DATE_RFC3339) ?></updated>
+        <summary type="text"><?= htmlspecialchars($article->description ?? '', ENT_XML1 | ENT_COMPAT, 'UTF-8') ?></summary>
+        <content type="html"><![CDATA[<?= $article->content ?>]]></content>
+        <author>
+            <name><?= htmlspecialchars('Dennis Koch', ENT_XML1 | ENT_COMPAT, 'UTF-8') ?></name>
+            <uri>https://denniskoch.dev</uri>
+        </author>
+    </entry>
+<?php endforeach; ?>
 </feed>
