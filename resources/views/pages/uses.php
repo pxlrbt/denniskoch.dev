bb0abdc47bcf5cc35e78bc56d658d0493b633d7e
Dennis Koch <info@pixelarbeit.de>
2026-04-11 17:41:25 +0200

    Fix double quotes around headline

---
+++ resources/views/pages/uses.php
@@ -0,0 +1,116 @@
+<?php
+    $title = '/uses | Dennis Koch';
+    $description = 'Freelance Developer specializing in Filament and Laravel for custom web applications. With over 12 years of experience, I create tailored solutions that meet unique business needs.';
+    $keywords = 'Freelance Developer, Full-Stack-Developer, Filament, Laravel, PHP, custom web applications, tailored solutions';
+?>
+
+<?php include __DIR__.'/../partials/header.php'; ?>
+
+    <article class="section wrapper">
+
+        <h1>/uses</h1>
+
+        <h2>Apps</h2>
+
+        <h3>Coding</h3>
+
+        <ul>
+            <li>PhpStorm <small>with Laravel IDEA &  Xdebug</small></li>
+            <li>VS Code (for lightweight work)</li>
+            <li>Cobalt2 theme</li>
+
+            <li>Laravel Herd</li>
+            <li>Table Plus</li>
+            <li>Transmit</li>
+            <li>Arc</li>
+            <li>Fish Shell</li>
+        </ul>
+
+        <h3>General</h3>
+
+        <ul>
+            <li>Raycast</li>
+            <li>Mail + Raycast Extension</li>
+            <li>Toggl</li>
+            <li>Spotify</li>
+            <li>Capacities</li>
+            <li>Reader</li>
+            <li>Figma</li>
+            <li>1Password</li>
+        </ul>
+
+        <h3>Utility</h3>
+
+        <ul>
+            <li>AltTab</li>
+            <li>Swish</li>
+            <li>Hazel</li>
+            <li>Bartender</li>
+            <li>Cleanshot</li>
+            <li>Contraste</li>
+            <li>Vivid</li>
+            <li>Keka</li>
+            <li>ImageOptim</li>
+            <li>Backblaze</li>
+        </ul>
+
+        <h3>Social</h3>
+
+        <ul>
+            <li>Signal</li>
+            <li>Ivory</li>
+            <li>Discord</li>
+            <li>Slack</li>
+            <li>Teams</li>
+        </ul>
+
+        <h3>Personal</h3>
+
+        <ul>
+            <li>Mela</li>
+        </ul>
+
+        <h2>Raycast Extensions</h2>
+
+        <ul>
+            <li>Calculator</li>
+            <li>Base64</li>
+            <li>Check End Of Life</li>
+            <li>Color Picker</li>
+            <li>Heroicons Search</li>
+            <li>Lorem Ipsum</li>
+            <li>Search Lucide Icons Search</li>
+            <li>Raycast Focus (Raycast)</li>
+            <li>Search Emoji and Symbols (Raycast)</li>
+            <li>Audio: Set Ouput Device (Raycast)</li>
+            <li>Transmit</li>
+            <li>Timezone Buddy</li>
+            <li>Table Plus</li>
+            <li>Empty Trash</li>
+            <li>Week Number</li>
+            <li>One-Time-Secret</li>
+        </ul>
+
+        <h2>Services</h2>
+
+        <ul>
+            <li>Hetzner</li>
+            <li>Ploi.io</li>
+            <li>Sentry</li>
+            <li>Cal.com</li>
+            <li>LemonSqueezy</li>
+            <li>Umami</li>
+        </ul>
+
+        <h2>Aliases / Commands</h2>
+
+        <pre>
+            <code>
+
+            </code>
+        </pre>
+
+
+    </article>
+
+<?php include __DIR__.'/../partials/footer.php'; ?>
